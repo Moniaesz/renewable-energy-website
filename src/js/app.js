407378db486aa9604ae73d6dd0befdfd82b7d304
Monika <molinskamonika+github@gmail.com>
2018-10-19 14:00:45 +0200

    mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const header = document.getElementsByTagName("header")[0];
     const menu = header.getElementsByClassName("menu")[0];
+    const nav = header.querySelector("nav");
     function openMobile() {
         menu.classList.toggle("open");
+        header.classList.toggle("show-menu");
+        nav.classList.toggle("show-nav");
     }
 
     menu.addEventListener("click", function() {
